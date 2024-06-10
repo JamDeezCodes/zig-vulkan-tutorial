c89aeebfbdba6b34bb3f2ab5a17ea94aa4706e1a
JolliestJames <cloudismydog@gmail.com>
2024-06-09 21:09:34 -0700

    intro, overview, development environment. am I a graphics programmer yet?

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,68 @@
 const std = @import("std");
+const glfw = @import("mach-glfw");
+const vk = @import("vulkan");
+const mach = @import("mach");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const vec4 = mach.math.vec4;
+const mat4 = mach.math.mat4x4;
+const Vec4 = mach.math.Vec4;
+const Mat4x4 = mach.math.Mat4x4;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+/// Default GLFW error handling callback
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+const BaseDispatch = vk.BaseWrapper(&.{.{
+    .base_commands = .{
+        .createInstance = true,
+        .enumerateInstanceExtensionProperties = true,
+        .getInstanceProcAddr = true,
+    },
+}});
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+
+    if (!glfw.init(.{})) {
+        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+
+    defer glfw.terminate();
+
+    const extent = vk.Extent2D{ .width = 800, .height = 600 };
+
+    const window = glfw.Window.create(
+        extent.width,
+        extent.height,
+        "Vulkan window",
+        null,
+        null,
+        .{
+            .client_api = .no_api,
+        },
+    ) orelse {
+        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+
+    const base_dispatch = try BaseDispatch.load(@as(
+        vk.PfnGetInstanceProcAddr,
+        @ptrCast(&glfw.getInstanceProcAddress),
+    ));
+
+    var extension_count: u32 = 0;
+    _ = try base_dispatch.enumerateInstanceExtensionProperties(null, &extension_count, null);
+
+    const matrix: Mat4x4 = undefined;
+    const vec = Vec4.splat(0);
+
+    _ = matrix.mulVec(&vec);
+
+    defer window.destroy();
+
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+    }
 }
